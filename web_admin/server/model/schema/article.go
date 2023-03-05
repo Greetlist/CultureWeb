392258c7540571993025031f0362ea4c0880dcd9
Greetlist <454281389@qq.com>
2023-03-05 20:08:36 +0800

    add: add comment orm

--- web_admin/server/model/schema/article.go
+++ web_admin/server/model/schema/article.go
@@ -4,16 +4,37 @@ import (
     "time"
 )
 
+type Comment struct {
+    CommentID uint `gorm:"column:comment_id;primary_key;AUTO_INCREMENT;" json:"comment_id"`
+    ArticleID uint `gorm:"column:article_id;not null;" json:"ariticle_id"`
+    UserID uint `gorm:"column:user_id;not null" json:"user_id"`
+    Content string `gorm:"column:content;not null;" json:"content"`
+    CreateTime time.Time `gorm:"column:create_time;autoCreateTime:milli;" json:"create_time"`
+    UpdateTime time.Time `gorm:"column:update_time;autoUpdateTime:milli;" json:"update_time"`
+}
+
 type Article struct {
     ArticleID uint `gorm:"column:article_id;primary_key;AUTO_INCREMENT;" json:"article_id"`
-    Rank uint `gorm:"column:rank;default:0;" json:"rank"`
     Title string `gorm:"column:title;not null;" json:"title"`
+    Rank uint `gorm:"column:rank;default:0;" json:"rank"`
+    Summary string `gorm:"column:summary;not null;" json:"summary"`
     Content string `gorm:"column:content;not null;" json:"content"`
     Lables []Label `gorm:"many2many:labels;" json:"labels"`
+    Comments []Comment `gorm:"foreignKey:CommentID" json:"comments"`
+    State string `gorm:"column:state;default:normal;" json:"state"`
+    Author string `gorm:"column:author;not null;" json:"author"`
+    IsEnable bool `gorm:"column:is_enable;not null;" json:"is_enable"`
+    IsTop bool `gorm:"column:is_top;not null;" json:"is_top"`
+    IsDelete bool `gorm:"column:is_delete;not null;" json:"is_delete"`
+    VisitNumber uint `gorm:"column:visit_number;not null;" json:"visit_number"`
     CreateTime time.Time `gorm:"column:create_time;autoCreateTime:milli;" json:"create_time"`
     UpdateTime time.Time `gorm:"column:update_time;autoUpdateTime:milli;" json:"update_time"`
 }
 
 func (uv Article) TableName() string {
     return "article"
+}
+
+func (c Comment) TableName() string {
+    return "comment"
 }
