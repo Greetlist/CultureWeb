cac46318aec4015ed52c109ca9954e7ab770fb65
Lisa <hels13@chinaunicom.cn>
2023-04-08 22:34:29 +0800

    refactor: routes of admin pages

---
+++ web_front/src/layout/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-container>
+    <el-header height="45px">
+      <Navbar title="XXX文化馆"></Navbar>
+    </el-header>
+    <el-container>
+      <el-aside width="220px">
+        <Sidebar></Sidebar>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import { Navbar, Sidebar } from "@c/index";
+export default {
+  name: "LayoutMain",
+  components: {
+    Navbar,
+    Sidebar,
+  },
+};
+</script>
+<style lang="scss" scoped>
+/deep/ {
+  .el-header {
+    padding: 0;
+  }
+  .el-aside {
+    a {
+      text-decoration: none;
+    }
+  }
+  .el-menu {
+    border-right: none;
+  }
+  .el-scrollbar {
+    height: calc(100vh - 45px);
+    border-right: solid 1px #e6e6e6;
+  }
+  .el-scrollbar__bar.is-vertical > div {
+    display: none;
+  }
+  .el-scrollbar__wrap {
+    overflow-x: hidden;
+  }
+  .el-main {
+    height: calc(100vh - 45px);
+  }
+}
+</style>
--- web_front/src/router/index.js
+++ web_front/src/router/index.js
@@ -1,99 +1,217 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import LoginView from "@views/Login.vue";
+import Layout from "@/layout/index.vue";
+import OverviewIndex from "@/views/Admin/Overview/index.vue";
+import SystemIndex from "@/views/Admin/SystemConfig/index.vue";
+import ArticalIndex from "@/views/Admin/Artical/index.vue";
+import ActivityIndex from "@/views/Admin/Activities/index.vue";
+import SiteIndex from "@/views/Admin/Site/index.vue";
+import UserIndex from "@/views/Admin/UserManage/index.vue";
 
 Vue.use(VueRouter);
+
+const adminRoutes = {
+  path: "/admin",
+  name: "admin",
+  redirect: "/admin/overview/userOverview",
+  component: Layout,
+  children: [
+    {
+      path: "/admin/overview",
+      name: "overview",
+      component: OverviewIndex,
+      meta: {
+        title: "总览",
+        icon: "el-icon-pie-chart",
+      },
+      children: [
+        {
+          path: "userOverview",
+          name: "userOverview",
+          component: () => import("@/views/Admin/Overview/User.vue"),
+          meta: {
+            title: "用户总览",
+          },
+        },
+        {
+          path: "visitOverview",
+          name: "visitOverview",
+          component: () => import("@/views/Admin/Overview/Visit.vue"),
+          meta: {
+            title: "访问总览",
+          },
+        },
+        {
+          path: "storageOverview",
+          name: "storageOverview",
+          component: () => import("@/views/Admin/Overview/Storage.vue"),
+          meta: {
+            title: "存储总览",
+          },
+        },
+      ],
+    },
+    {
+      path: "/admin/systemConfig",
+      name: "overview",
+      component: SystemIndex,
+      meta: {
+        title: "系统设置",
+        icon: "el-icon-setting",
+      },
+      children: [
+        {
+          path: "baseInfo",
+          name: "baseInfo",
+          component: () => import("@/views/Admin/SystemConfig/BaseInfo.vue"),
+          meta: {
+            title: "基本信息",
+          },
+        },
+        {
+          path: "baseConfig",
+          name: "baseConfig",
+          component: () => import("@/views/Admin/SystemConfig/BaseConfig.vue"),
+          meta: {
+            title: "参数设置",
+          },
+        },
+      ],
+    },
+    {
+      path: "/admin/userManage",
+      name: "userManage",
+      component: UserIndex,
+      meta: {
+        title: "用户设置",
+        icon: "el-icon-user",
+      },
+      children: [
+        {
+          path: "totalUserList",
+          name: "totalUserList",
+          component: () => import("@/views/Admin/UserManage/TotalUserList.vue"),
+          meta: {
+            title: "所有用户",
+          },
+        },
+        {
+          path: "addUser",
+          name: "addUser",
+          component: () => import("@/views/Admin/UserManage/AddUser.vue"),
+          meta: {
+            title: "新增用户",
+          },
+        },
+      ],
+    },
+    {
+      path: "/admin/articalManage",
+      name: "articalManage",
+      component: ArticalIndex,
+      meta: {
+        title: "文章管理",
+        icon: "el-icon-reading",
+      },
+      children: [
+        {
+          path: "articalList",
+          name: "articalList",
+          component: () => import("@/views/Admin/Artical/ArticalList.vue"),
+          meta: {
+            title: "文章列表",
+          },
+        },
+        {
+          path: "addArtical",
+          name: "addArtical",
+          component: () => import("@/views/Admin/Artical/AddArtical.vue"),
+          meta: {
+            title: "新增文章",
+          },
+        },
+      ],
+    },
+    {
+      path: "/admin/activityManage",
+      name: "activityManage",
+      component: ActivityIndex,
+      meta: {
+        title: "活动管理",
+        icon: "el-icon-alarm-clock",
+      },
+      children: [
+        {
+          path: "activitiesList",
+          name: "activitiesList",
+          component: () =>
+            import("@/views/Admin/Activities/ActivitiesList.vue"),
+          meta: {
+            title: "活动列表",
+          },
+        },
+        {
+          path: "addActivity",
+          name: "addActivity",
+          component: () => import("@/views/Admin/Activities/AddActivity.vue"),
+          meta: {
+            title: "新增活动",
+          },
+        },
+      ],
+    },
+    {
+      path: "/admin/siteManage",
+      name: "siteManage",
+      component: SiteIndex,
+      meta: {
+        title: "场馆预约管理",
+        icon: "el-icon-location-outline",
+      },
+      children: [
+        {
+          path: "siteManagementLis",
+          name: "siteManagementLis",
+          component: () => import("@/views/Admin/Site/SiteList.vue"),
+          meta: {
+            title: "所有预约",
+          },
+        },
+        {
+          path: "siteManagementList",
+          name: "siteManagementList",
+          component: () => import("@/views/Admin/Site/AddNewSite.vue"),
+          meta: {
+            title: "新增场地",
+          },
+        },
+      ],
+    },
+  ],
+};
 
 const routes = [
   {
     path: "/",
     name: "login",
     component: LoginView,
+    hidden: true,
   },
   {
     path: "/home",
     name: "home",
     component: () => import("@/views/User/Home.vue"),
     children: [],
+    hidden: true,
   },
-  {
-    path: "/admin",
-    name: "admin",
-    component: () => import("@/views/Admin/Home.vue"),
-    children: [
-      {
-        path: "/userOverview",
-        name: "userOverview", // 用户总览
-        component: () => import("@/views/Admin/Overview/User.vue"),
-      },
-      {
-        path: "/visitOverview",
-        name: "visitOverview", // 访问总览
-        component: () => import("@/views/Admin/Overview/Visit.vue"),
-      },
-      {
-        path: "/storageOverview",
-        name: "storageOverview", // 存储总览
-        component: () => import("@/views/Admin/Overview/Storage.vue"),
-      },
-      {
-        path: "/baseInfo",
-        name: "baseInfo", // 基本信息
-        component: () => import("@/views/Admin/SystemConfig/BaseInfo.vue"),
-      },
-      {
-        path: "/baseConfig",
-        name: "baseConfig", // 基本信息
-        component: () => import("@/views/Admin/SystemConfig/BaseConfig.vue"),
-      },
-      {
-        path: "/totalUserList",
-        name: "totalUserList", // 所有用户
-        component: () => import("@/views/Admin/UserManage/TotalUserList.vue"),
-      },
-      {
-        path: "/addUser",
-        name: "addUser", // 新增用户
-        component: () => import("@/views/Admin/UserManage/AddUser.vue"),
-      },
-      {
-        path: "/articalList",
-        name: "articalList", // 文章列表
-        component: () => import("@/views/Admin/Artical/ArticalList.vue"),
-      },
-      {
-        path: "/addArtical",
-        name: "addArtical", // 新增文章
-        component: () => import("@/views/Admin/Artical/AddArtical.vue"),
-      },
-      {
-        path: "/activitiesList",
-        name: "activitiesList", // 活动列表
-        component: () => import("@/views/Admin/Activities/ActivitiesList.vue"),
-      },
-      {
-        path: "/addActivity",
-        name: "addActivity", // 新增活动
-        component: () => import("@/views/Admin/Activities/AddActivity.vue"),
-      },
-      {
-        path: "/siteManagementLis",
-        name: "siteManagementLis", // 所有预约
-        component: () => import("@/views/Admin/Site/SiteList.vue"),
-      },
-      {
-        path: "/addNewSite",
-        name: "addNewSite", // 新增场地
-        component: () => import("@/views/Admin/Site/AddNewSite.vue"),
-      },
-    ],
-  },
+  adminRoutes,
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes,
 });
 
 export default router;
+export { adminRoutes };
