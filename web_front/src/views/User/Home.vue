869c08ef543952b9568d5835904a4e29b8983303
Greetlist <454281389@qq.com>
2023-06-07 13:36:08 +0800

    add width height test

--- web_front/src/views/User/Home.vue
+++ web_front/src/views/User/Home.vue
@@ -1,13 +1,44 @@
 <template>
-  <h1>HomePage</h1>
+  <div>
+    Type: {{ type }}, Width: {{ width }}, Height: {{ height }}
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
   data() {
-    return {};
+    return {
+      type: '',
+      width: '',
+      height: ''
+    };
   },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth < 500) {
+        this.type = "mobile"
+      } else if (window.innerWidth >= 500 && window.innerWidth <= 1200) {
+        this.type = "pad"
+      } else {
+        this.type = "computer"
+      }
+      this.width = window.innerWidth
+      this.height = window.innerHeight
+    }
+  },
+  created() {
+    this.onResize()
+  }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
